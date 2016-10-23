0a96defa7b9ca8264d09aa428fa790799ad58724
Mirim An <mimicat@Mirims-MacBook-Air.local>
2016-10-22 23:30:08 -0400

    Done

--- PickinFruit/AppDelegate.swift
+++ PickinFruit/AppDelegate.swift
@@ -16,29 +16,35 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        print("🍊🍊🍊🍊🍊did finish lanching with options🍊🍊🍊🍊🍊")
         return true
     }
 
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+        print("🍎 🍎 🍎 application will resign active🍎 🍎 🍎")
     }
 
     func applicationDidEnterBackground(_ application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+        print("🍇 🍇 🍇 app did enter background 🍇 🍇 🍇")
     }
 
     func applicationWillEnterForeground(_ application: UIApplication) {
         // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+        print("🍎🍎 🍎🍎 app will enter foreground🍎🍎 🍎🍎")
     }
 
     func applicationDidBecomeActive(_ application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+        print(" 🍊  🍊 app did become active 🍊  🍊")
     }
 
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+        print("🍇 🍇 🍇 app will terminate🍇 🍇 🍇")
     }
 
 
--- PickinFruit/ViewController.swift
+++ PickinFruit/ViewController.swift
@@ -1,33 +1,83 @@
-//
-//  ViewController.swift
-//  PickinFruit
-//
-//  Created by Flatiron School on 7/17/16.
-//  Copyright © 2016 Flatiron School. All rights reserved.
-//
-
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
 
-    @IBOutlet weak var spinButton: UIButton!
     @IBOutlet weak var resultLabel: UILabel!
+    @IBOutlet weak var spinButton: UIButton!
     
+    @IBOutlet weak var fruitPicker: UIPickerView!
+
     var fruitsArray = ["🍎", "🍊", "🍌", "🍐", "🍇", "🍉", "🍓", "🍒", "🍍"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        print(" 🍎 viewDidLoad 🍇")
+        resultLabel.isHidden = true
         self.spinButton.accessibilityLabel = Constants.SPIN_BUTTON
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print(" 🍊 viewWillAppear 🍇")
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        print(" 🍌 viewDidAppear 🍇")
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        print(" 🍐 viewDidDisappear 🍇 ")
+        // these are not for apps entering background 
         
     }
     
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        print(" 🍇 viewDidDisappear 🍇")
+    }
+    
+    
     @IBAction func spinButtonTapped(_ sender: UIButton) {
         
+    fruitPicker.selectRow(Int(arc4random_uniform(UInt32(fruitsArray.count))), inComponent: 0, animated: true)
         
+    fruitPicker.selectRow(Int(arc4random_uniform(UInt32(fruitsArray.count))), inComponent: 1, animated: true)
         
+    fruitPicker.selectRow(Int(arc4random_uniform(UInt32(fruitsArray.count))), inComponent: 2, animated: true)
+        
+    outcome()
+    
     }
-
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 3
+    }
+    
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return fruitsArray.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        print("row is:\(row)")
+       return fruitsArray[row]
+       
+    }
+    
+    func outcome(){
+        let fruit1 = fruitPicker.selectedRow(inComponent: 0)
+        let fruit2 = fruitPicker.selectedRow(inComponent: 1)
+        let fruit3 = fruitPicker.selectedRow(inComponent: 2)
+        
+        if fruit1 == fruit2 && fruit2 == fruit3 {
+            resultLabel.isHidden = false
+            resultLabel.text = "WINNER!"
+        } else {
+            resultLabel.isHidden = false
+            resultLabel.text = "TRY AGAIN"
+        }
+    }
 }
 
 // MARK: Set Up
@@ -48,6 +98,7 @@ extension ViewController {
     }
     
 }
+
 
 
 
